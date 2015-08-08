767fa5870513c04d101224ba24172c44ed0369ce
BrendanDayley <brendandayley@gmail.com>
2015-08-08 09:13:01 -0600

    pushing a few changes from last night

--- server.js
+++ server.js
@@ -14,53 +14,67 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	res.header('Access-Controll-Allow-Origin', '*');
 	res.header('Access-Controll-Allow-Methods', 'OPTIONS, GET, POST');
 	res.header('Access-Contoll-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
 });
 
-app.get('/name', function(req, res){
+app.get('/name', function (req, res) {
 	console.log(JSON.stringify(name));
 	res.send(JSON.stringify(name))
 });
-app.get('/location', function(req, res){
+app.get('/location', function (req, res) {
 	res.send(JSON.stringify(location));
 });
-app.get('/hobbies', function(req, res){
-	if(req.query.order === 'asc'){
-		res.send({hobbies: hobbies.sort()});
-	}else if (req.query.order === 'desc'){
-		res.send({hobbies: hobbies.sort().reverse()});
-	}else{
-	res.send(JSON.stringify({hobbies: hobbies}));
-}
-});
-app.get('/occupations', function(req, res){
-	if(req.query.order === 'asc'){
-		res.send({occupations: occupations.sort()});
-	}else if (req.query.order === 'desc'){
-		res.send({occupations: occupations.sort().reverse()});
-	}else{
-	res.send(JSON.stringify({Occupations: occupations}));
+app.get('/hobbies', function (req, res) {
+	if (req.query.order === 'asc') {
+		res.send({
+			hobbies: hobbies.sort()
+		});
+	} else if (req.query.order === 'desc') {
+		res.send({
+			hobbies: hobbies.sort().reverse()
+		});
+	} else {
+		res.send(JSON.stringify({
+			hobbies: hobbies
+		}));
 	}
 });
-app.get('/occupations/latest', function(req, res){
-	res.send(JSON.stringify({mostRecentOccupation: occupations[occupations.length - 1]}))
+app.get('/occupations', function (req, res) {
+	if (req.query.order === 'asc') {
+		res.send({
+			occupations: occupations.sort()
+		});
+	} else if (req.query.order === 'desc') {
+		res.send({
+			occupations: occupations.sort().reverse()
+		});
+	} else {
+		res.send(JSON.stringify({
+			Occupations: occupations
+		}));
+	}
+});
+app.get('/occupations/latest', function (req, res) {
+	res.send(JSON.stringify({
+		mostRecentOccupation: occupations[occupations.length - 1]
+	}))
 });
 
-app.put('/name', function(req, res){
-	name = 'req.params.name'
+app.put('/name', function (req, res) {
+	name = req.body.name;
 });
-app.put('/location', function(req, res){
-	
+app.put('/location', function (req, res) {
+
 });
-app.put('/hobbies', function(req, res){
-	
+app.put('/hobbies', function (req, res) {
+
 });
-app.put('/occupations', function(req, res){
-	
+app.put('/occupations', function (req, res) {
+
 });
 
 app.listen(8181);
